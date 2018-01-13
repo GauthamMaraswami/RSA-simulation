7661cbba9270f52136dfafe8db7c0b17502d0890
Gdarkstar <15co118.gautham@nitk.edu.in>
2018-01-13 08:20:14 +0530

    prime sieve

--- prime_utils.cpp
+++ prime_utils.cpp
@@ -147,16 +147,47 @@ return true;
 
 //prime_utils.cpp
 
+struct primes{
+		int list[1000000];
+		int size;
+};
 
+struct primes seive(int n){
+    bool prime[n+1];
+    memset(prime, true, sizeof(prime));
+  	for (int p=2; p*p<=n; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*2; i<=n; i += p)
+                prime[i] = false;
+        }
+    }
+	struct primes ans;
+		ans.size=0;
 
+	for(int p=2;p<=n;p++)
+	{
+		if(prime[p]==true)
+		{
+			ans.list[ans.size]=p;
+			ans.size++;	
+			cout<<ans.list[ans.size-1]<<endl ;	
+		}
+	}
+return ans;
+//return 0;
+}
 
 
 int main()
 {
 //cout<<fermats_test(561);
 //cout<<miller_rabin(29);
-
-cout<<solovay_strassen(221);
+struct primes  res=seive(54);
+//cout<<solovay_strassen(221);
 //struct xgcda ans =recursive_xgcd(15, 35);
 //cout<<ans.gcd<<ans.x<<ans.y<<endl;
 //cout<<recursive_gcd(10,25);
